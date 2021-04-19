4c48c5bbf8c1f62cb9f5c0bf67580aa8280c37e2
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2021-04-19 19:14:20 +0300

    Add description of Git task

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains:annotations:16.0.2")
+    implementation("commons-codec:commons-codec:1.11") // утилиты для хеширования
+    implementation("commons-io:commons-io:2.6") // утилиты для работы с IO
+    implementation("commons-cli:commons-cli:1.4") // фреймворк для создания CLI
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
@@ -24,6 +27,7 @@ java {
         }
         test {
             java.setSrcDirs(listOf("src/test"))
+            resources.setSrcDirs(listOf("src/testResources"))
         }
     }
 }
